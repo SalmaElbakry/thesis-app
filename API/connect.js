5f56522eb65e642fbd5d29a8a5f8c652495f6150
salmamoh01 <salma5511@hotmail.com>
2023-04-26 20:48:55 +0200

    register and login backend + fixing some errors

--- API/connect.js
+++ API/connect.js
@@ -1,8 +1,9 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost ",
+  host: "localhost",
   user: "root",
   password: "Thesis.salma24",
   database: "thesis",
+  port:"8000"
 });
--- API/index.js
+++ API/index.js
@@ -1,16 +1,25 @@
-import Express from "express";
-const app = Express();
+import express from "express";
+import bcryptjs from "bcryptjs";
+const app = express();
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/likes", likeRoutes);
-app.use("/api/auths", authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(8800, () => {
   console.log("API working!");
