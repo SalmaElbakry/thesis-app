808847a30fc822c1ba08d503d7a95ec27360ace8
salmamoh01 <salma5511@hotmail.com>
2023-04-26 16:09:34 +0200

    Backend & API setup + boiler text

---
+++ API/controllers/auth.js
@@ -0,0 +1,9 @@
+export const register = (req, res) => {
+  // Check if user exists
+  // Create new user
+  // Hash password
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
---
+++ API/index.js
@@ -0,0 +1,17 @@
+import Express from "express";
+const app = Express();
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/auths", authRoutes);
+
+app.listen(8800, () => {
+  console.log("API working!");
+});
