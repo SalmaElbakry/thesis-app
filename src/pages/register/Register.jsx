d9632715d5fb09c0e8a0d27073696476d7ad4e3d
salmamoh01 <salma5511@hotmail.com>
2023-04-27 09:17:17 +0200

    backend register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./register.scss"
+import  axios  from "axios";
 
 const Register = () => {
+
+  const [inputs, setInputs] = useState({
+     username:  "",
+     email:  "",
+     password:  "",
+     name:  "",
+  })
+
+  const [err, setErr] = useState(null);
+
+  // the names at the bottom (in input) to their values (in the use state part)
+  const handleChange = e => {
+    setInputs(prev => ({...prev, [e.target.name]:e.target.value}))
+  }
+  // works
+  // console.log(inputs)
+
+  const handleClick = async e =>{
+    // so it does not refresh the page
+    e.preventDefault();
+
+    
+    try{
+      await axios.post("http://localhost:8800/api/auth/register", inputs)
+    } catch (err){
+      setErr(err.response.data);
+    }
+  }
+
+  console.log(err);
+
   return (
     <div className="register">
     <div className="card">
@@ -17,11 +49,12 @@ const Register = () => {
       <div className="right">
         <h1>Register</h1>
         <form >
-          <input type="text" placeholder='Username' />
-          <input type="email" placeholder='Email'/>
-          <input type="password" placeholder='Password'/>
-          <input type="text" placeholder='Name'/>
-          <button>Register</button>
+          <input type="text" placeholder='Username' name = "username" onChange={handleChange}/>
+          <input type="email" placeholder='Email' name = "email" onChange={handleChange}/>
+          <input type="password" placeholder='Password' name = "password" onChange={handleChange}/>
+          <input type="text" placeholder='Name' name = "name" onChange={handleChange}/>
+          {err && err}
+          <button onClick={handleClick}>Register</button>
         </form>
       </div>
     </div>
